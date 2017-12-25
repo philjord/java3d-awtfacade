d57563a6d6cff7dbb76df62740728b00543db929
phil <philjord@ihug.co.nz>
2017-12-26 11:26:34 +1300

    Graphics and font moved forwards but not up to shim identical

--- java3dawtfacade/src/javaawt/Color.java
+++ java3dawtfacade/src/javaawt/Color.java
@@ -5,4 +5,11 @@ public class Color
 	public float r;
 	public float g;
 	public float b;
+	public float a;
+	public Color(int r, int g, int b, int a) {
+		this.r=r;
+		this.g=g;
+		this.b=b;
+		this.a=a;
+	}
 }
